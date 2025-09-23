63cd0a495d98f1be3961b9ede01145184c412893
Ahmed Zaytoon <citybirdman@gmail.com>
2025-09-23 06:48:55 +0000

    update market price report

--- mobility_reports/mobility_reports/report/market_price/market_price.py
+++ mobility_reports/mobility_reports/report/market_price/market_price.py
@@ -3,9 +3,9 @@ import pandas as pd # type: ignore
 
 
 def execute(filters=None):
-	data = get_data(filters)
-	columns=get_column(data)
-	return columns, data
+    data = get_data(filters)
+    columns=get_column(data)
+    return columns, data
 
 
 def get_column(data):
@@ -32,67 +32,68 @@ def get_column(data):
 
 def get_data(filters):
     price_list=filters.get("price_list")
-	from_date_filter = "1=1"
-	if filters and filters.get("from_date"):
-		from_date = filters["from_date"]
-		from_date_filter = f" creation >= '{from_date}'"
+    from_date_filter = "1=1"
+    if filters and filters.get("from_date"):
+        from_date = filters["from_date"]
+        from_date_filter = f" creation >= '{from_date}'"
   
-	
-	production_year_filter='and 1=1'
-	if filters and filters.get("production_year"):
-		from_year = filters["production_year"]
-		production_year_filter=f" and production_year='{from_year}'"
-	brand_filter='and 1=1'
-	if filters and filters.get("brand"):
-		from_brand = filters["brand"]
-		brand_filter=f" and item.brand='{from_brand}'"
+    
+    production_year_filter='and 1=1'
+    if filters and filters.get("production_year"):
+        from_year = filters["production_year"]
+        production_year_filter=f" and production_year='{from_year}'"
+        
+    brand_filter='and 1=1'
+    if filters and filters.get("brand"):
+        from_brand = filters["brand"]
+        brand_filter=f" and item.brand='{from_brand}'"
      
-	item_price=frappe.db.sql(f"""
-			select item.name, item.item_name,
-			item.brand,
-			item.country_of_origin,
-			item.tire_size, 
-			item.ply_rating,
-			ip.production_year,
-			ip.valid_from,
-			ip.price_list_rate as `price`
-			from `tabItem`as item 
-			join `tabItem Price` as ip on item.name = ip.item_code
-			where item_group = "Tires"
-			and ip.price_list='{price_list}'
-			{production_year_filter}
-			{brand_filter}
+    item_price=frappe.db.sql(f"""
+            select item.name, item.item_name,
+            item.brand,
+            item.country_of_origin,
+            item.tire_size, 
+            item.ply_rating,
+            ip.production_year,
+            ip.valid_from,
+            ip.price_list_rate as `price`
+            from `tabItem`as item 
+            join `tabItem Price` as ip on item.name = ip.item_code
+            where item_group = "Tires"
+            and ip.price_list='{price_list}'
+            {production_year_filter}
+            {brand_filter}
                        """,as_dict=True)
-	item_price   = [dict(row) for row in item_price]
-	item_price= pd.DataFrame(item_price)
+    item_price   = [dict(row) for row in item_price]
+    item_price= pd.DataFrame(item_price)
  
-	market_price=frappe.db.sql(f"""
-				select market_brand,
-				creation,
-				origin as country_of_origin,
-				distributor,
-				tire_size,
-				pr as ply_rating,
-				price,
-				production_year
-				from `tabItem Market Price` 
-    			where {from_date_filter} 
-       			{production_year_filter}
+    market_price=frappe.db.sql(f"""
+                select market_brand,
+                creation,
+                origin as country_of_origin,
+                distributor,
+                tire_size,
+                pr as ply_rating,
+                price,
+                production_year
+                from `tabItem Market Price` 
+                where {from_date_filter} 
+                   {production_year_filter}
                          """,as_dict=True)
-	market_price   = [dict(row) for row in market_price]
-	market_price=pd.DataFrame(market_price)
+    market_price   = [dict(row) for row in market_price]
+    market_price=pd.DataFrame(market_price)
  
-	market_price=market_price.sort_values(['creation'])
-	
-	df=item_price.merge(market_price,how='left',on=['tire_size','ply_rating','country_of_origin']).fillna('0')
+    market_price=market_price.sort_values(['creation'])
+    
+    df=item_price.merge(market_price,how='left',on=['tire_size','ply_rating','country_of_origin']).fillna('0')
  
-	df['distributor/brand']=df['distributor']+' / '+df['market_brand']
+    df['distributor/brand']=df['distributor']+' / '+df['market_brand']
  
-	pivot_df=df.pivot_table(index=['name','brand','tire_size','ply_rating','country_of_origin','Arabian Price']\
+    pivot_df=df.pivot_table(index=['name','brand','tire_size','ply_rating','country_of_origin','Arabian Price']\
                          ,columns=['distributor/brand'],values='price',aggfunc='last',observed=True).reset_index().infer_objects(copy=False).fillna(0)
  
-	if '0 / 0' in pivot_df.columns:
-		pivot_df.drop(columns=['0 / 0'], inplace=True)
-	
-	return pivot_df.to_dict(orient='records')
+    if '0 / 0' in pivot_df.columns:
+        pivot_df.drop(columns=['0 / 0'], inplace=True)
+    
+    return pivot_df.to_dict(orient='records')
     
